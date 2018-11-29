d4b153c1814f7e8814c1b2ae7a933ff71064b492
sherlynlobo <sherlynlobo26@gmail.com>
2018-11-29 14:14:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sherlynlobo/sreeram_sherlyn_project/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sherlynlobo.sreeram_sherlyn_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
